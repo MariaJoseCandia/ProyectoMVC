7df09a0edb4d89139e02e4d0bfb6177da66969ec
MariaJoseCandia <mariajosecandia100@gmail.com>
2021-11-10 15:56:56 -0300

    Update controllers

--- Escuela.Mvc/Controllers/CursoController.cs
+++ Escuela.Mvc/Controllers/CursoController.cs
@@ -1,3 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Escuela.Core;
 
-namespace 
+namespace Escuela.Controllers
+{
+    public class CursoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+         => View(Repositorio.Curso); 
+
+        [HttpGet]
+        public IActionResult FormAlta() => View();
+
+        [HttpPost]
+        public IActionResult FormAlta(Curso curso)
+        {
+            Repositorio.AgregarCurso(curso);
+            return View("Index", Repositorio.Curso);
+        }
+
+    }
+}
